6605d28bea177a0f12c64f26d979ef569ae018c0
Sarthak <sarthakvkumar@gmail.com>
2024-01-07 11:44:44 +0530

    Integrated APIs and Initiated Admin Panel

--- backend/index.js
+++ backend/index.js
@@ -73,9 +73,21 @@ const Product = mongoose.model("Product", {
     },
 })
 
+// API for creating product
+
 app.post('/addproduct', async (req, res) => {
+
+    let products = await Product.find({});
+    let id;
+    if (products.length > 0) {
+        let last_product_array = products.slice(-1);
+        let last_product = last_product_array[0];
+        id = last_product.id + 1;
+    } else {
+        id = 1;
+    }
     const product = new Product({
-        id: req.body.id,
+        id: id,
         name: req.body.name,
         image: req.body.image,
         category: req.body.category,
@@ -90,6 +102,27 @@ app.post('/addproduct', async (req, res) => {
         name: req.body.name
     });
 })
+
+// API for deleting product
+
+app.post('/removeproduct', async (req, res) => {
+    await Product.findOneAndDelete({ id: req.body.id });
+    console.log("Removed");
+    res.json({
+        success: true,
+        name: req.body.name
+    })
+})
+
+
+// API for finding all products
+
+app.post('/allproducts', async (req, res) => {
+    let products = await Product.find({});
+    console.log("All products fetched");
+    res.send(products)
+})
+
 
 app.listen(port, (error) => {
     if (!error)
