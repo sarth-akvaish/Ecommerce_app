e5c836b92f1bd5f65b7d494223535032bd19ae2c
Sarthak <sarthakvkumar@gmail.com>
2024-01-08 21:11:48 +0530

    multer errors fixed

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ app.get("/", (req, res) => {
 const storage = multer.diskStorage({
     destination: './upload/images',
     filename: (req, file, cb) => {
-        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+        return cb(null, Date.now() + '-' + file.originalname);
     }
 })
 
@@ -37,9 +37,12 @@ const upload = multer({ storage: storage });
 app.use('/images', express.static('upload/images'));
 
 app.post("/upload", upload.single('product'), (req, res) => {
+    if (!req.file) {
+        return res.status(400).json({ error: "No file Uploaded" });
+    }
     res.json({
         success: 1,
-        image_url: `http://localhost:${process.env.PORT}/images/${req.file.filename}`
+        message: "File uploaded Successfully"
     })
 })
 
